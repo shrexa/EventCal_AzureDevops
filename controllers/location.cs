94f628402347f50955aed5070e264c04497583de
SHRIYA CHOUDHARY <shriya.choudhary9876@gmail.com>
2024-04-16 01:37:19 +0530

    get, put {id}

--- controllers/location.cs
+++ controllers/location.cs
@@ -58,6 +58,77 @@ namespace Events.Calendar.Controllers
             return Ok(locationsModel);
         }
         
+        //PUT: api/Location/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLocation(Guid id, ModelNs.LocationModel Location)
+        {
+            if (id != Location.Id)
+            {
+                return BadRequest();
+            }
+
+            var LocationDb = await _context.Location.FindAsync(id);
+            if (LocationDb == null)
+            {
+                return BadRequest();
+            }
+            LocationDb.Name = Location.Name;
+            LocationDb.Code = Location.Code;
+            LocationDb.CountryId = Location.CountryId;
+            LocationDb.ModifiedOn = DateTime.UtcNow;
+            LocationDb.ModifiedBy = "System";
+            LocationDb.RowVersion = BitConverter.GetBytes(Convert.ToUInt64(DateTime.UtcNow.Ticks));
+
+            _context.Location.Update(LocationDb);
+            _context.Entry(LocationDb).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LocationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+
+        //POST: api/Location
+        [HttpPost]
+        public async Task<ActionResult<ModelNs.LocationModel>> PostLocation(ModelNs.LocationModel Location)
+        {
+            var LocationDb = new DomainNs.Location
+            {
+                Id = Location.Id,
+                Name = Location.Name,
+                Code = Location.Code,
+                Description = Location.Description,
+                StatusCode = DomainNs.StatusCodeEnum.Active,
+                StatusSubCode = DomainNs.StatusSubCodeEnum.New,
+                CreatedOn = DateTime.UtcNow,
+                CreatedBy = "System",
+                RowVersion = BitConverter.GetBytes(Convert.ToUInt64(DateTime.UtcNow.Ticks))
+            };
+            _context.Location.Add(LocationDb);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLocation", new { id = Location.Id }, Location);
+        }
+
+
+        private bool LocationExists(Guid id)
+        {
+            return _context.Location.Any(e => e.Id == id);
+        }
 
 
         //PUT: api/Location/5
