ad917e6a4fb7b83c4006bd0873f9b09b6857b1a3
SHRIYA CHOUDHARY <shriya.choudhary9876@gmail.com>
2024-04-18 02:32:42 +0530

    error fixed

--- controllers/location.cs
+++ controllers/location.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Events.Calendar.Data;
 using ModelNs = Events.Calendar.Model;
 using DomainNs = Events.Calendar.Domain;
+using Events.Calendar.Model;
 
 namespace Events.Calendar.Controllers
 {
@@ -29,18 +30,22 @@ namespace Events.Calendar.Controllers
         public async Task<ActionResult<IEnumerable<ModelNs.LocationModel>>> GetLocation()
         {
             var locations = await _context.Location.ToListAsync();
-            var locationsModel = locations.Select(c2 => new ModelNs.LocationModel()
+            var locationsModel = locations.Select(c3 => new ModelNs.LocationModel()
             {
-                Id = c2.Id,
-                Name = c2.Name,
-                IsoCode = c2.IsoCode
-            });
+                Id = c3.Id,
+                Name = c3.Name,
+                Code = c3.IsoCode,
+                MapsLongitude = c3.MapsLongitude,
+                MapsLatitude = c3.MapsLatitude,
+                Description = c3.Description
+             });
             return Ok(locationsModel);
         }
 
+
         // GET: api/Location/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<ModelNs.LocationModel>> GetLocation(Guid id)
+        [HttpGet("{id}")]  //attribute constructor
+        public async Task<ActionResult<ModelNs.LocationModel>> GetLocation(Guid id) //get loctn is a method of Attribute Constructor
         {
             var Location = await _context.Location.FindAsync(id);
 
@@ -49,15 +54,22 @@ namespace Events.Calendar.Controllers
                 return NotFound();
             }
 
-            var locationsModel = new ModelNs.LocationModel()
+            var locationsModel = new ModelNs.LocationModel() //LocationModel is class
             {
                 Id = Location.Id,
                 Name = Location.Name,
-                IsoCode = Location.IsoCode
+                Code = Location.IsoCode,
+                CityId = Location.CityId,
+                Description = Location.Description,
+                //CountryId = Location.CountryId,
+                MapsLongitude = Location.MapsLongitude,
+                MapsLatitude = Location.MapsLatitude,
             };
             return Ok(locationsModel);
         }
-        
+
+
+
         //PUT: api/Location/5
         [HttpPut("{id}")]
         public async Task<IActionResult> PutLocation(Guid id, ModelNs.LocationModel Location)
@@ -73,8 +85,8 @@ namespace Events.Calendar.Controllers
                 return BadRequest();
             }
             LocationDb.Name = Location.Name;
-            LocationDb.Code = Location.Code;
-            LocationDb.CountryId = Location.CountryId;
+            LocationDb.IsoCode = Location.Code;
+            //LocationDb.CountryId = Location.CountryId;
             LocationDb.ModifiedOn = DateTime.UtcNow;
             LocationDb.ModifiedBy = "System";
             LocationDb.RowVersion = BitConverter.GetBytes(Convert.ToUInt64(DateTime.UtcNow.Ticks));
@@ -103,87 +115,17 @@ namespace Events.Calendar.Controllers
 
 
         //POST: api/Location
-        [HttpPost]
+        [HttpPost] 
         public async Task<ActionResult<ModelNs.LocationModel>> PostLocation(ModelNs.LocationModel Location)
         {
             var LocationDb = new DomainNs.Location
             {
                 Id = Location.Id,
                 Name = Location.Name,
-                Code = Location.Code,
+                IsoCode = Location.Code,
                 Description = Location.Description,
-                StatusCode = DomainNs.StatusCodeEnum.Active,
-                StatusSubCode = DomainNs.StatusSubCodeEnum.New,
-                CreatedOn = DateTime.UtcNow,
-                CreatedBy = "System",
-                RowVersion = BitConverter.GetBytes(Convert.ToUInt64(DateTime.UtcNow.Ticks))
-            };
-            _context.Location.Add(LocationDb);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetLocation", new { id = Location.Id }, Location);
-        }
-
-
-        private bool LocationExists(Guid id)
-        {
-            return _context.Location.Any(e => e.Id == id);
-        }
-
-
-        //PUT: api/Location/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutLocation(Guid id, ModelNs.LocationModel Location)
-        {
-            if (id != Location.Id)
-            {
-                return BadRequest();
-            }
-
-            var LocationDb = await _context.Location.FindAsync(id);
-            if (LocationDb == null)
-            {
-                return BadRequest();
-            }
-            LocationDb.Name = Location.Name;
-            LocationDb.IsoCode = Location.IsoCode;
-            LocationDb.CountryId = Location.CountryId;
-            LocationDb.ModifiedOn = DateTime.UtcNow;
-            LocationDb.ModifiedBy = "System";
-            LocationDb.RowVersion = BitConverter.GetBytes(Convert.ToUInt64(DateTime.UtcNow.Ticks));
-
-            _context.Location.Update(LocationDb);
-            _context.Entry(LocationDb).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!LocationExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-
-        //POST: api/Location
-        [HttpPost]
-        public async Task<ActionResult<ModelNs.LocationModel>> PostLocation(ModelNs.LocationModel Location)
-        {
-            var LocationDb = new DomainNs.Location
-            {
-                //Id = Location.Id,
-                Name = Location.Name,
-                IsoCode = Location.IsoCode,
+                MapsLongitude = Location.MapsLongitude,
+                MapsLatitude = Location.MapsLatitude,
                 StatusCode = DomainNs.StatusCodeEnum.Active,
                 StatusSubCode = DomainNs.StatusSubCodeEnum.New,
                 CreatedOn = DateTime.UtcNow,
